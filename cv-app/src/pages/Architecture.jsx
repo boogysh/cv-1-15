53a8b5b8f93301d529a4071468afa78d02772976
Victor Buga <bugavictor86@gmail.com>
2025-09-20 09:19:49 +0200

    architecture_page_fix_h2_fontsize_query

--- cv-app/src/pages/Architecture.jsx
+++ cv-app/src/pages/Architecture.jsx
@@ -9,7 +9,7 @@ export default function Architecture() {
   return (
     <main className="flex flex-col items-center bg-bg_body  main-scroll">
       <Banner src={banner} title={t.archTitle} />
-      <h2 className="w-full text-center font-black text-[30px] lg:text-[44px] p-10 mt-3">Mes réalisations pendant mes études à la faculté d'Urbanisme et d'Architecture </h2>
+      <h2 className="w-full text-center font-black text-[24px] md:text-[30px] lg:text-[44px] p-4 md:p-8 lg:pd-10 mt-3">Mes réalisations pendant mes études à la faculté d'Urbanisme et d'Architecture </h2>
       <section
         id="cards"
         className="flex flex-wrap justify-center pt-3 md:pt-5 lg:pt-7 2xl:pt-10"
