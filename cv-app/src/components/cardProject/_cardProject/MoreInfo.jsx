cd723e260e8f280eff0425aa960157b9fe0dd12e
Victor Buga <bugavictor86@gmail.com>
2025-10-19 16:41:52 +0200

    fix card project more info

--- cv-app/src/components/cardProject/_cardProject/MoreInfo.jsx
+++ cv-app/src/components/cardProject/_cardProject/MoreInfo.jsx
@@ -11,12 +11,12 @@ export default function LinkToProjectDevNew(props) {
         props.moreInfo && "translate-y-0-percents"
       } transition-transform ease-in-out duration-300 w-full h-full flex flex-col justify-center items-center cursor-pointer bg-[#ebdede]`}
     >
-      <div className="flex flex-wrap p-3 pr-10  justify-center">
-        <ul className="pr-2  s:p-5 pt-0">
+      <div className="relative h-auto  flex flex-wrap p-3 justify-center overflow-hidden">
+        <ul className=" pr-2 h-[100%]  p-5 pt-0  overflow-scroll ">
           {props.info.map((item) => (
             <li className="list-disc" key={uuidv4()}>
-              <p className="text-[11px] font-sans leading-[12px] s:leading-4  s:text-[14px] mb-[5px]">
-                {" "}
+              <p className="text-[11px] font-sans w-auto  xxs:text-[12px] xs:text-[13px]  s:leading-4  s:text-[14px] mb-[0px]  s:mb-[5px] ">
+                
                 {item}
               </p>
             </li>
