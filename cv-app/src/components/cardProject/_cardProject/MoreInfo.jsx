a23f2436bd87b8bb12ba47eb52dcfa02a54cc692
Victor Buga <bugavictor86@gmail.com>
2025-10-21 21:03:56 +0200

    fix moreinfo scroll

--- cv-app/src/components/cardProject/_cardProject/MoreInfo.jsx
+++ cv-app/src/components/cardProject/_cardProject/MoreInfo.jsx
@@ -9,10 +9,10 @@ export default function LinkToProjectDevNew(props) {
     <div
       className={`absolute top-0 left-0 translate-y-100-percents ${
         props.moreInfo && "translate-y-0-percents"
-      } transition-transform ease-in-out duration-300 w-full h-full flex flex-col justify-center items-center cursor-pointer bg-[#ebdede]`}
+      } transition-transform ease-in-out duration-300 w-full h-full flex flex-col justify-center items-center  bg-[#ebdede]`}
     >
       <div className="relative h-auto  flex flex-wrap p-3 justify-center overflow-hidden">
-        <ul className=" pr-2 h-[100%]  p-5 pt-0  overflow-scroll ">
+        <ul className=" pr-2 h-[100%]  p-5 pt-0  overflow-scroll  sm:overflow-auto">
           {props.info.map((item) => (
             <li className="list-disc" key={uuidv4()}>
               <p className="text-[11px] font-sans w-auto  xxs:text-[12px] xs:text-[13px]  s:leading-4  s:text-[14px] mb-[0px]  s:mb-[5px] ">
