6fa7ee48ddbaa9940ceaa6e34fa0c85a57569517
Victor Buga <bugavictor86@gmail.com>
2025-11-10 17:44:51 +0100

    secutity => 10000

--- cv-back/middlewares/security.js
+++ cv-back/middlewares/security.js
@@ -25,7 +25,7 @@ const corsOptions = {
 // ✅ Limiteur de requêtes (100 requêtes / 15 min / IP)
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 2000,
+  max: 10000,
   standardHeaders: true,
   legacyHeaders: false,
   message: "Trop de requêtes depuis cette IP. Réessayez plus tard.",
