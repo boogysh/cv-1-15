103e46833758670e5b2a4d74163d18a4a5689910
Victor Buga <bugavictor86@gmail.com>
2025-11-20 21:49:20 +0100

    sparkles => 120

--- cv-app/src/components/cardProject/_cardProject/StarSparkle.jsx
+++ cv-app/src/components/cardProject/_cardProject/StarSparkle.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./StarSparkle.css";
 
 export default function StarSparkle({ color }) {
-  const sparks = Array.from({ length:  200});
+  const sparks = Array.from({ length:  120});
 
   return (
     <div className="spark-explode-zone">
