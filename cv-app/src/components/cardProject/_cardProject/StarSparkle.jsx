8fbc0cd9291e6bfd0a49071b72456eb0bce24cc7
Victor Buga <bugavictor86@gmail.com>
2025-11-20 21:39:30 +0100

    star 500 > 200 & add css will-change

--- cv-app/src/components/cardProject/_cardProject/StarSparkle.jsx
+++ cv-app/src/components/cardProject/_cardProject/StarSparkle.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./StarSparkle.css";
 
 export default function StarSparkle({ color }) {
-  const sparks = Array.from({ length: 500 });
+  const sparks = Array.from({ length:  200});
 
   return (
     <div className="spark-explode-zone">
